822eb68dbd0a647503110339beb8bb59d5e9db11
emonn1667 <emonn1667@gmail.com>
2024-01-23 02:58:43 +0600

    normal design done

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    }
+  }
+
+  /** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
